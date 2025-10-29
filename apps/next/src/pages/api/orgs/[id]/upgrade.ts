6aa72078a4de03ce14cdd655ee06ff2775ab7857
vlizumrud10043-a11y <vl.izumrud10043@gmail.com>
2025-10-29 13:49:11 +0200

    Update upgrade.ts

--- apps/next/src/pages/api/orgs/[id]/upgrade.ts
+++ apps/next/src/pages/api/orgs/[id]/upgrade.ts
@@ -29,7 +29,7 @@ export default async function handler(
     return res.status(402).json({ error: "Payment required" });
 
   let org = await prisma.organization.findFirst({
-    where: { metadata: { path: "$.paymentId", equals: session_id } },
+    where: { metadata: { path: "$.paymentId", arrayContains: session_id} },
   });
 
   const member = await prisma.organizationMembership.findFirst({
